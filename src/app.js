43768f880833ff5d33e243abb1e3c4d0a8d1d836
Jaladurgam-Sumanth <sumanth.jaladurgam@meeamitech.com>
2025-04-02 14:28:50 +0530

    cors access enabled

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 import express from 'express';
-
+import http from 'http';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+const server = http.createServer(app);
 
 app.use(express.json());
 
 
-export { app };
+export { app, server };
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import { app } from './app.js';
 
 app.listen(8504, () => {
     console.log("Server is running on port 8504");
+    console.log('WebSocket', 'WebSocket server is ready');
 });
